7de6a185f50f8b9323c5019f6bd27058d8fbae4d
tngye <tngyeefong@gmail.com>
2022-11-16 01:24:23 +0800

    edit

--- server/src/main/java/com/nus/iss/server/security/SecurityConfigurer.java
+++ server/src/main/java/com/nus/iss/server/security/SecurityConfigurer.java
@@ -54,7 +54,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/#/*", "/authenticate", "/register/**", "/api/**").permitAll().
+				.authorizeRequests().antMatchers("/**", "/authenticate", "/register/**", "/api/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
